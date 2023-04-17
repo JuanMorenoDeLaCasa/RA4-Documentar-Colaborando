9955c46945f2996bdc418715e97b31c8f825d4bb
Juan Moreno <usuariojuanito@gmail.com>
2023-04-17 19:53:42 +0200

    Version 0.2

--- calculadora/Suma.java
+++ calculadora/Suma.java
@@ -1,36 +1,69 @@
+
+/**
+
+ Este es un código de la clase Suma de la calculadora.
+ Esta es la versión 0.0.2
+ */
 package calculadora;
-
-
 public class Suma {
-	
-	public float valorAcumulado;
-	
-	public float getValorAcumulado() {
-		return valorAcumulado;
-	}
+/**
+ * Atributo para almacenar el valor acumulado de las operaciones de suma.
+ */
+public float valorAcumulado;
 
-	public void setValorAcumulado(float valorAcumulado) {
+/**
+ * Método para obtener el valor acumulado de las operaciones de suma.
+ * @return valorAcumulado El valor acumulado de las operaciones de suma.
+ */
+public float getValorAcumulado() {
+		return valorAcumulado;
+		}
+
+/**
+ * Método para establecer el valor acumulado de las operaciones de suma.
+ * @param valorAcumulado El valor acumulado de las operaciones de suma.
+ */
+public void setValorAcumulado(float valorAcumulado) {
 		this.valorAcumulado = valorAcumulado;
-	}
-	
-	
-	public float dosReales(float a, float b) {
+		}
+
+/**
+ * Método para sumar dos números reales y devolver el resultado.
+ * @param a El primer número real.
+ * @param b El segundo número real.
+ * @return La suma de a y b.
+ */
+public float dosReales(float a, float b) {
 		return a+b;
-	}
-	
-	public int dosEnteros(int a, int b) {
+		}
+
+/**
+ * Método para sumar dos números enteros y devolver el resultado.
+ * @param a El primer número entero.
+ * @param b El segundo número entero.
+ * @return La suma de a y b.
+ */
+public int dosEnteros(int a, int b) {
 		return a+b;
-	}
-	
-	public float tresReales(float a, float b, float c) {
+		}
+
+/**
+ * Método para sumar tres números reales y devolver el resultado.
+ * @param a El primer número real.
+ * @param b El segundo número real.
+ * @param c El tercer número real.
+ * @return La suma de a, b y c.
+ */
+public float tresReales(float a, float b, float c) {
 		return a+b+c;
-	}
-	
-	public float sumaAcumulado(float a) {
+		}
+
+/**
+ * Método para sumar un número real al valor acumulado y devolver el resultado.
+ * @param a El número real que se va a sumar.
+ * @return El nuevo valor acumulado de las operaciones de suma.
+ */
+public float sumaAcumulado(float a) {
 		valorAcumulado = a + valorAcumulado;
 		return valorAcumulado;
-	}
-}
-
-
-
+		}
